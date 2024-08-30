e2352a0e246b815445a16d55ec1b9b2006bd3f1b
andrevinni <andre.vinicius.coelho@gmail.com>
2024-08-30 17:55:44 -0300

    commit aula configuration-properties

--- src/main/java/dio/springboot/app/SistemaMensagem.java
+++ src/main/java/dio/springboot/app/SistemaMensagem.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 //import java.util.Arrays;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
@@ -11,21 +12,15 @@ import org.springframework.stereotype.Component;
 @Component
 public class SistemaMensagem implements CommandLineRunner {
 
-	@Value("${nome:NoReply-DIO}")
-	private String nome;
-	
-	@Value("${email}")
-	private String email;
-	
-	@Value("${telefones}")
-	private List<Long> telefones = new ArrayList<>();
+	@Autowired
+	private Remetente remetente;
 	
 	@Override
 	public void run(String... args) throws Exception {
 		// TODO Auto-generated method stub
-		System.out.println("Mensagem enviada por: " + nome 
-				+ "\nE-mail: " + email 
-				+ "\nCom Telefones para contato: " + telefones);
+		System.out.println("Mensagem enviada por: " +  remetente.getNome()
+				+ "\nE-mail: " + remetente.getEmail()
+				+ "\nCom Telefones para contato: " + remetente.getTelefones());
 		System.out.println("Seu cadastro foi aprovado");
 		
 	}
